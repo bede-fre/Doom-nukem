0a2f7a067fac98619aa92bd83cd62602766bbc74
Thomas OLIVER <toliver@e1r4p4.42.fr>
2018-09-18 14:14:06 +0200

    gneuh

--- includes/vectors.h
+++ includes/vectors.h
@@ -6,13 +6,27 @@
 /*   By: toliver <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/18 12:40:23 by toliver           #+#    #+#             */
-/*   Updated: 2018/09/18 12:43:34 by toliver          ###   ########.fr       */
+/*   Updated: 2018/09/18 14:11:43 by toliver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VECTORS_H
 #define VECTORS_H
 
+#include <math.h>
 
+typedef struct	s_vec
+{
+	float		x;
+	float		y;
+	float		z;
+}				t_vec;
+
+float		ft_degtorad(float angle);
+float		ft_radtodeg(float angle);
+
+t_vec		ft_vecdef(float x, float y, float z);
+t_vec		ft_vecadd(t_vec a, t_vec b);
+t_vec		ft_vecsub(t_vec a, t_vec b);
 
 #endif
--- srcs/vector/vector.c
+++ srcs/vector/vector.c
@@ -6,8 +6,48 @@
 /*   By: toliver <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/18 12:40:01 by toliver           #+#    #+#             */
-/*   Updated: 2018/09/18 12:43:32 by toliver          ###   ########.fr       */
+/*   Updated: 2018/09/18 14:13:28 by toliver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vectors.h"
+
+float		ft_degtorad(float angle)
+{
+	return (angle / 180.0 * M_PI);
+}
+
+float		ft_radtodeg(float angle)
+{
+	return (angle * 180 / M_PI);
+}
+
+t_vec		ft_vecdef(float x, float y, float z)
+{
+	t_vec	v;
+
+	v.x = x;
+	v.y = y;
+	v.z = z;
+	return (v);
+}
+
+t_vec		ft_vecadd(t_vec a, t_vec b)
+{
+	t_vec	v;
+
+	v.x = a.x + b.x;
+	v.y = a.y + b.y;
+	v.z = a.z + b.z;
+	return (v);
+}
+
+t_vec		ft_vecsub(t_vec a, t_vec b)
+{
+	t_vec	v;
+
+	v.x = a.x - b.x;
+	v.y = a.y - b.y;
+	v.z = a.z - b.z;
+	return (v);
+}
