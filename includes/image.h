257a481e7292f332ef6cf311466d6742f8ed8035
Thibault BERTHIER <tberthie@e3r8p3.42.fr>
2018-09-20 19:28:55 +0200

    Image merge function

---
+++ includes/image.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   image.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tberthie <tberthie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/20 19:25:59 by tberthie          #+#    #+#             */
+/*   Updated: 2018/09/20 19:27:05 by tberthie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IMAGE_H
+# define IMAGE_H
+
+typedef struct		s_img
+{
+	void			*ptr;
+	char			*data;
+
+	int				width;
+	int				height;
+
+}					t_img;
+
+# define BYTES_PER_PIXEL	4
+
+#endif
--- srcs/tools/images.c
+++ srcs/tools/images.c
@@ -6,38 +6,40 @@
 /*   By: tberthie <tberthie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/20 16:38:43 by tberthie          #+#    #+#             */
-/*   Updated: 2018/09/20 17:02:32 by tberthie         ###   ########.fr       */
+/*   Updated: 2018/09/20 19:28:10 by tberthie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doom.h"
 
-typedef struct	s_img
-{
-	void		*ptr;
-	char		*data;
-
-	int			width;
-	int			height;
-
-}				t_img;
-
-#define BYTES_PER_PIXEL 4
-
 void			merge_images(t_img *dest, t_img *src, int x, int y)
 {
 	char		*dest_data;
+	char		*src_data;
 	int			i;
 
 	i = 0;
-	dest_data = dest->data + (y * dest->width + x) * BYTES_PER_PIXEL;
+	if (x >= dest->width || y >= dest->height)
+		return ;
+	dest_data = dest->data + ((y >= 0 ? y : 0) * dest->width +
+		(x >= 0 ? x : 0)) * BYTES_PER_PIXEL;
+	src_data = src->data;
 	while (i < src->height)
 	{
-		memcpy(dest_data, src_data, (src->width > dest->width - x ?
-				dest->width - x : src->width) * BYTES_PER_PIXEL);
+		if (y >= 0 || i + y >= 0)
+		{
+			if (x < 0 && x + src->width > 0)
+				memcpy(dest_data, src_data - (x * BYTES_PER_PIXEL), 
+				(src->width + x > dest->width ?
+					dest->width : src->width + x) * BYTES_PER_PIXEL);
+			else if (x >= 0)
+				memcpy(dest_data, src_data, (src->width > dest->width - x ?
+					dest->width - x : src->width) * BYTES_PER_PIXEL);
+		}
 		if (++i + y >= dest->height)
 			break;
-		dest_data += (dest->width + x) * BYTES_PER_PIXEL;
+		if (i + y > 0)
+			dest_data += dest->width * BYTES_PER_PIXEL;
 		src_data += src->width * BYTES_PER_PIXEL;
 	}
 }
