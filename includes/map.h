eef17eb337a64beedaf445d76c54c1d765a101ad
Thomas OLIVER <toliver@e1r5p11.42.fr>
2018-09-24 15:23:05 +0200

    first map to test

--- includes/map.h
+++ includes/map.h
@@ -6,7 +6,7 @@
 /*   By: tberthie <tberthie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/18 15:29:52 by tberthie          #+#    #+#             */
-/*   Updated: 2018/09/18 16:14:06 by tberthie         ###   ########.fr       */
+/*   Updated: 2018/09/24 15:22:41 by toliver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ typedef struct			s_wall
 {
 	char				type; // clip / noclip / ...?
 
+	int					nextzone;
 	t_vec				origin;
 	t_vec				direction;
 
--- srcs/core/main.c
+++ srcs/core/main.c
@@ -6,11 +6,44 @@
 /*   By: toliver <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/20 17:32:23 by toliver           #+#    #+#             */
-/*   Updated: 2018/09/20 19:49:02 by toliver          ###   ########.fr       */
+/*   Updated: 2018/09/24 15:22:37 by toliver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doom.h"
+
+t_wall				*wall(char type, int nextzone, t_vec origin, t_vec dir)
+{
+	t_wall			*ptr;
+
+	ptr = (t_wall*)malloc(sizeof(t_wall));
+	ptr->type = type;
+	ptr->nextzone = nextzone;
+	ptr->origin = origin;
+	ptr->direction = dir;
+	return (ptr);
+}
+
+t_zone				*malloczonetest(void)
+{
+	t_zone			*zone;
+
+	zone = (t_zone*)malloc(sizeof(t_zone));
+	zone->walls = (t_wall**)malloc(sizeof(t_wall*) * 5);
+	zone->walls[0] = wall(0, -1, ft_vecdef(0, 0, 0), ft_vecdef(1, 0, 0));
+	zone->walls[1] = wall(0, -1, ft_vecdef(20, 0, 0), ft_vecdef(0, 1, 0));
+	zone->walls[2] = wall(0, -1, ft_vecdef(20, 20, 0), ft_vecdef(20, -1, 0));
+	zone->walls[3] = wall(0, -1, ft_vecdef(0, 20, 0), ft_vecdef(-1, 0, 0));
+	zone->walls[4] = NULL;
+	return (zone);
+}
+
+static void			mapinit(t_doom *env)
+{
+	env->zones = (t_zone**)malloc(sizeof(t_zone*) * 2);
+	env->zones[1] = NULL;
+	env->zones[0] = malloczonetest();
+}
 
 static void			playerinit(t_doom *env)
 {
@@ -33,6 +66,7 @@ static void			init(t_doom **env)
 	playerinit(doom);
 	doom->param.sensitivity = 1.0;
 	doom->param.mousepos = ft_vecdef(-6000, -6000, 0);
+	mapinit(doom);
 	*env = doom;
 }
 
@@ -48,7 +82,7 @@ int					main(void)
 {
 	t_doom			*env;
 	init(&env);
-	blueskintest();
+//	blueskintest();
 	loop(env);
 	return (0);
 }
