f44c78114fea066dfc70a3ebb25bfa201d4cdcd0
Adrien RUELLOU <aruellou@student.42.fr>
2018-11-14 17:59:45 +0100

    autopush

--- srcs/game/srcs/movements2.c
+++ srcs/game/srcs/movements2.c
@@ -5,25 +5,26 @@ void	jump_and_crouch(t_all *all)
 	static int jump = FALSE;
 
 	if (all->keys_tab[KEY_CTRL] && all->wall_gap1 > (4.0f / 3.0f)
-			&& all->wall_gap2 < 4.0f
-					&& all->wall_gap1 <= 2.0 && all->wall_gap2 >= 2.0f) {
+	&& all->wall_gap2 < 4.0f & all->wall_gap1 <= 2.0 && all->wall_gap2 >= 2.0f)
+	{
 		all->wall_gap1 -= (2.0 - (4.0f / 3.0f)) / TRANS_F;
 		all->wall_gap2 += (4.0f - 2.0f) / TRANS_F;
-	} else if (!all->keys_tab[KEY_CTRL] && all->wall_gap1 < 2.0f
-			&& all->wall_gap2 > 2.0f) {
+	}
+	else if (!all->keys_tab[KEY_CTRL] && all->wall_gap1 < 2.0f)
+	{
 		all->wall_gap1 += (2.0 - (4.0f / 3.0f)) / TRANS_F;
 		all->wall_gap2 -= (4.0f - 2.0f) / TRANS_F;
 	}
-	else if (jump) {
+	else if (jump)
+	{
 		all->wall_gap1 += 0.2f;
 		all->wall_gap2 -= 0.067f;
-		if (all->wall_gap1 >= 4.0f && all->wall_gap2 <= 1.33f)
-			jump = FALSE;
+		jump = (all->wall_gap1 >= 4.0f && all->wall_gap2 <= 1.33f) ? FALSE : TRUE;
 	}
-	else if (all->keys_tab[KEY_SPACEBAR] && all->wall_gap1 == 2.0f
-			&& all->wall_gap2 == 2.0f && !jump)
+	else if (all->keys_tab[KEY_SPACEBAR] && all->wall_gap1 == 2.0f && !jump)
 		jump = TRUE;
-	else if (all->wall_gap1 > 2.0f && all->wall_gap2 < 2.0f && !jump) {
+	else if (all->wall_gap1 > 2.0f && all->wall_gap2 < 2.0f && !jump)
+	{
 		all->wall_gap1 -= 0.2f;
 		all->wall_gap2 += 0.067f;
 	}
