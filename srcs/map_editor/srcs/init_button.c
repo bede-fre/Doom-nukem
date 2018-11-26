162f46a54e15ebe73907b3a823ecf27326b85bee
Lucas GUILLEROT <lguiller@e1r2p13.42.fr>
2018-11-26 17:40:05 +0100

    descente de touts les boutton et texte d'une case

--- srcs/map_editor/srcs/init_button.c
+++ srcs/map_editor/srcs/init_button.c
@@ -6,7 +6,7 @@
 /*   By: cmace <cmace@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/14 10:07:18 by lguiller          #+#    #+#             */
-/*   Updated: 2018/11/26 12:15:59 by bede-fre         ###   ########.fr       */
+/*   Updated: 2018/11/26 17:36:58 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,13 +36,13 @@ static void		init_big_button(t_env *env)
 	SDL_Rect	rect;
 	const int	dim = SCALE * 5;
 
-	rect = create_rect(map_to_win(36) + 2, map_to_win(2) + 2, dim, dim);
+	rect = create_rect(map_to_win(36) + 2, map_to_win(3) + 2, dim, dim);
 	env->buttons[WOOD] = create_button(rect, BLACK, BLACK, env->text.t_a);
-	rect = create_rect(map_to_win(42) + 2, map_to_win(2) + 2, dim, dim);
+	rect = create_rect(map_to_win(42) + 2, map_to_win(3) + 2, dim, dim);
 	env->buttons[METAL] = create_button(rect, BLACK, BLACK, env->text.t_b);
-	rect = create_rect(map_to_win(36) + 2, map_to_win(8) + 2, dim, dim);
+	rect = create_rect(map_to_win(36) + 2, map_to_win(9) + 2, dim, dim);
 	env->buttons[STONE] = create_button(rect, BLACK, BLACK, env->text.t_c);
-	rect = create_rect(map_to_win(42) + 2, map_to_win(8) + 2, dim, dim);
+	rect = create_rect(map_to_win(42) + 2, map_to_win(9) + 2, dim, dim);
 	env->buttons[ICE] = create_button(rect, BLACK, BLACK, env->text.t_d);
 }
 
@@ -51,23 +51,23 @@ void			init_button(t_env *env)
 	SDL_Rect	rect;
 	const int	dim = SCALE;
 
-	rect = create_rect(map_to_win(38), map_to_win(20), dim, dim);
+	rect = create_rect(map_to_win(38), map_to_win(21), dim, dim);
 	env->buttons[TP_START] = create_button(rect, TP_S_COL, BLACK, NULL);
-	rect = create_rect(map_to_win(44), map_to_win(20), dim, dim);
+	rect = create_rect(map_to_win(44), map_to_win(21), dim, dim);
 	env->buttons[TP_END] = create_button(rect, TP_E_COL, BLACK, NULL);
-	rect = create_rect(map_to_win(38), map_to_win(24), dim, dim);
+	rect = create_rect(map_to_win(38), map_to_win(25), dim, dim);
 	env->buttons[B_START] = create_button(rect, START_COL, BLACK, NULL);
-	rect = create_rect(map_to_win(38), map_to_win(16), dim, dim);
+	rect = create_rect(map_to_win(38), map_to_win(17), dim, dim);
 	env->buttons[COLISION] = create_button(rect, BLACK, BLACK, NULL);
-	rect = create_rect(map_to_win(44), map_to_win(16), dim, dim);
+	rect = create_rect(map_to_win(44), map_to_win(17), dim, dim);
 	env->buttons[NO_COLISION] = create_button(rect, BLACK, BLACK, NULL);
-	rect = create_rect(map_to_win(38), map_to_win(26), dim, dim);
+	rect = create_rect(map_to_win(38), map_to_win(27), dim, dim);
 	env->buttons[B_ERASER] = create_button(rect, BLACK, BLACK,
 		env->text.eraser);
-	rect = create_rect(map_to_win(44), map_to_win(24), dim, dim);
+	rect = create_rect(map_to_win(44), map_to_win(25), dim, dim);
 	env->buttons[B_DOOR] = create_button(rect, T_DOOR_COL, BLACK,
 		env->text.door);
-	rect = create_rect(map_to_win(44), map_to_win(26), dim, dim);
+	rect = create_rect(map_to_win(44), map_to_win(27), dim, dim);
 	env->buttons[B_END] = create_button(rect, END_COL, BLACK, NULL);
 	init_big_button(env);
 }
